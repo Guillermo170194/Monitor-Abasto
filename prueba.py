a1d5d755f162040f7f40996cccae55a883cdf326
guillermo ortega <memo170194@gmail.com>
2026-05-13 17:44:32 -0600

    actualizacion dashboard

--- prueba.py
+++ prueba.py
@@ -55,30 +55,416 @@ archivo = st.sidebar.file_uploader(
     type=["xlsb", "xlsx"]
 )
 
-if archivo is None:
-    st.warning("⬅ Debes cargar la consolidada.")
+archivo_inv = st.sidebar.file_uploader(
+    "Subir Inventario",
+    type=["xlsx", "xlsb"]
+)
+
+archivo_cpm = st.sidebar.file_uploader(
+    "Subir CPM",
+    type=["xlsx", "xlsb"]
+)
+if (
+    archivo is None or
+    archivo_inv is None or
+    archivo_cpm is None
+):
+    st.warning("⬅ Debes cargar todos los archivos.")
     st.stop()
 @st.cache_data(show_spinner=False)
 def cargar():
+    reemplazos = {
+        "MICHOACÁN": "MICHOACAN",
+        "MICHOACÁN DE OCAMPO": "MICHOACAN",
+        "MICHOACAN DE OCAMPO": "MICHOACAN",
+        "BAJA SUR": "BAJA CALIFORNIA SUR",
+        "B.C.S.": "BAJA CALIFORNIA SUR",
+        "BCS": "BAJA CALIFORNIA SUR"
+    }
     df = pd.read_excel(
         archivo,
         engine="pyxlsb",
-        usecols=[0, 3, 17, 24, 27, 55]
+        usecols=[0, 3, 17, 18, 24, 27, 55]
     )
     df.columns = df.columns.str.strip()
-    return df
+    # =========================
+    # INVENTARIO
+    # =========================
 
-df = cargar()
+    inv = pd.read_excel(
+        archivo_inv
+    )
 
+    inv.columns = inv.columns.str.strip()
+
+    col_inv_estado = inv.columns[0]
+    col_inv_clave = inv.columns[3]
+    col_inv_piezas = inv.columns[5]
+
+    inv["ENTIDAD"] = (
+        inv[col_inv_estado]
+        .astype(str)
+        .str.strip()
+        .str.upper()
+        .str.normalize("NFKD")
+        .str.encode("ascii", errors="ignore")
+        .str.decode("utf-8")
+    )
+
+    inv["ENTIDAD"] = (
+        inv["ENTIDAD"]
+        .replace(reemplazos)
+    )
+
+    inv["CLAVE"] = (
+        inv[col_inv_clave]
+        .astype(str)
+        .str.strip()
+    )
+
+    inv["CLAVE_MERGE"] = (
+        inv["CLAVE"]
+        .str.replace(".", "", regex=False)
+        .str.replace(" ", "", regex=False)
+    )
+
+    inv["PIEZAS_INV"] = (
+        inv[col_inv_piezas]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
+    # =========================
+    # FILTRAR INVENTARIO VIGENTE
+    # =========================
+
+    col_inv_estatus = inv.columns[7]
+
+    inv["ESTATUS"] = (
+        inv[col_inv_estatus]
+        .astype(str)
+        .str.strip()
+        .str.upper()
+    )
+
+    inv = inv[
+        ~inv["ESTATUS"].str.contains(
+            "CADUC",
+            na=False
+        )
+    ]
+    inv["PIEZAS_INV"] = pd.to_numeric(
+        inv["PIEZAS_INV"],
+        errors="coerce"
+    ).fillna(0)
+
+    # CONSOLIDAR INVENTARIO
+    inv_group = (
+        inv.groupby(
+            ["ENTIDAD", "CLAVE_MERGE"],
+            dropna=False
+        )["PIEZAS_INV"]
+        .sum()
+        .reset_index()
+    )
+    # =========================
+    # CPM
+    # =========================
+
+    cpm = pd.read_excel(
+        archivo_cpm
+    )
+
+    cpm.columns = cpm.columns.str.strip()
+
+    col_cpm_estado = cpm.columns[0]
+    col_cpm_clave = cpm.columns[1]
+    col_cpm = cpm.columns[3]
+
+    cpm["ENTIDAD"] = (
+        cpm[col_cpm_estado]
+        .astype(str)
+        .str.strip()
+        .str.upper()
+        .str.normalize("NFKD")
+        .str.encode("ascii", errors="ignore")
+        .str.decode("utf-8")
+    )
+
+    cpm["ENTIDAD"] = (
+        cpm["ENTIDAD"]
+        .replace(reemplazos)
+    )
+
+    cpm["CLAVE"] = (
+        cpm[col_cpm_clave]
+        .astype(str)
+        .str.strip()
+    )
+
+    cpm["CLAVE_MERGE"] = (
+        cpm["CLAVE"]
+        .str.replace(".", "", regex=False)
+        .str.replace(" ", "", regex=False)
+    )
+
+    cpm["CPM"] = (
+        cpm[col_cpm]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
+
+    cpm["CPM"] = pd.to_numeric(
+        cpm["CPM"],
+        errors="coerce"
+    ).fillna(0)
+
+    cpm_group = (
+        cpm.groupby(
+            ["ENTIDAD", "CLAVE_MERGE"],
+            dropna=False
+        )["CPM"]
+        .sum()
+        .reset_index()
+    )
+    # =========================
+        # =========================
+    # LIMPIAR CONSOLIDADA
+    # =========================
+
+    df["ENTIDAD"] = (
+        df[df.columns[1]]
+        .astype(str)
+        .str.strip()
+        .str.upper()
+        .str.normalize("NFKD")
+        .str.encode("ascii", errors="ignore")
+        .str.decode("utf-8")
+    )
+
+    df["ENTIDAD"] = (
+        df["ENTIDAD"]
+        .replace(reemplazos)
+    )
+
+    df["CLAVE"] = (
+        df[df.columns[2]]
+        .astype(str)
+        .str.strip()
+    )
+
+    df["CLAVE_MERGE"] = (
+        df["CLAVE"]
+        .str.replace(".", "", regex=False)
+        .str.replace(" ", "", regex=False)
+    )
+
+        # =========================
+    # LIMPIAR CONSOLIDADA
+    # =========================
+
+    df["ENTIDAD"] = (
+        df[df.columns[1]]
+        .astype(str)
+        .str.strip()
+        .str.upper()
+        .str.normalize("NFKD")
+        .str.encode("ascii", errors="ignore")
+        .str.decode("utf-8")
+        .str.replace(".", "", regex=False)
+    )
+
+    df["ENTIDAD"] = (
+        df["ENTIDAD"]
+        .replace(reemplazos)
+    )
+
+    df["CLAVE"] = (
+        df[df.columns[2]]
+        .astype(str)
+        .str.strip()
+        .str.replace(".", "", regex=False)
+        .str.replace(" ", "", regex=False)
+    )
+
+    # =========================
+    # MERGE INVENTARIO
+    # =========================
+
+    df = df.merge(
+        inv_group,
+        how="left",
+        on=["ENTIDAD", "CLAVE_MERGE"],
+    )
+
+    df["PIEZAS_INV"] = (
+        df["PIEZAS_INV"]
+        .fillna(0)
+    )
+
+    # =========================
+    # MERGE CPM
+    # =========================
+
+    df = df.merge(
+        cpm_group,
+        how="left",
+        on=["ENTIDAD", "CLAVE_MERGE"],
+    )
+
+    df["CPM"] = (
+        df["CPM"]
+        .fillna(0)
+    )
+
+    # =========================
+    # AGREGAR CLAVES SIN EMISIÓN
+    # =========================
+
+    base_extra = pd.merge(
+        inv_group,
+        cpm_group,
+        how="outer",
+        on=["ENTIDAD", "CLAVE_MERGE"]
+    )
+
+    existentes = set(
+        zip(df["ENTIDAD"], df["CLAVE_MERGE"])
+    )
+
+    base_extra = base_extra[
+        ~base_extra.apply(
+            lambda x: (
+                x["ENTIDAD"],
+                x["CLAVE_MERGE"]
+            ) in existentes,
+            axis=1
+        )
+    ]
+
+    if len(base_extra) > 0:
+
+        extra = pd.DataFrame({
+            df.columns[0]: "SIN EMISION",
+            df.columns[1]: base_extra["ENTIDAD"],
+            df.columns[2]: base_extra["CLAVE_MERGE"],	
+            df.columns[3]: "SIN DESCRIPCIÓN",
+            df.columns[4]: 0,
+            df.columns[5]: 0,
+            df.columns[6]: 0,
+            "PIEZAS_INV": base_extra["PIEZAS_INV"],
+            "CPM": base_extra["CPM"]
+        })
+
+        df = pd.concat(
+            [df, extra],
+            ignore_index=True
+        )
+    return df, inv_group, cpm_group
+
+df, inv_group, cpm_group = cargar()
 # =========================
 # COLUMNAS
 # =========================
 col_orden = df.columns[0]
 col_estado = df.columns[1]
 col_clave = df.columns[2]
-col_precio = df.columns[3]
-col_emitidas = df.columns[4]
-col_entregadas = df.columns[5]
+col_descripcion = df.columns[3]
+col_precio = df.columns[4]
+col_emitidas = df.columns[5]
+col_entregadas = df.columns[6]
+# =========================
+# BASE BUSCADOR
+# =========================
+
+df["PIEZAS_INV"] = pd.to_numeric(
+    df["PIEZAS_INV"],
+    errors="coerce"
+).fillna(0)
+
+df["CPM"] = pd.to_numeric(
+    df["CPM"],
+    errors="coerce"
+).fillna(0)
+base_busqueda = pd.merge(
+    inv_group,
+    cpm_group,
+    how="outer",
+    on=["ENTIDAD", "CLAVE_MERGE"]
+)
+
+base_busqueda["PIEZAS_INV"] = (
+    base_busqueda["PIEZAS_INV"]
+    .fillna(0)
+)
+
+base_busqueda["CPM"] = (
+    base_busqueda["CPM"]
+    .fillna(0)
+)
+
+emitido = (
+    df.groupby(
+        ["ENTIDAD", "CLAVE_MERGE"],
+        dropna=False
+    )[col_emitidas]
+    .sum()
+    .reset_index()
+)
+
+entregado = (
+    df.groupby(
+        ["ENTIDAD", "CLAVE_MERGE"],
+        dropna=False
+    )[col_entregadas]
+    .sum()
+    .reset_index()
+)
+
+descripcion = (
+    df.groupby(
+        ["ENTIDAD", "CLAVE_MERGE"],
+        dropna=False
+    )[col_descripcion]
+    .first()
+    .reset_index()
+)
+
+base_busqueda = base_busqueda.merge(
+    emitido,
+    how="left",
+    on=["ENTIDAD", "CLAVE_MERGE"]
+)
+
+base_busqueda = base_busqueda.merge(
+    entregado,
+    how="left",
+    on=["ENTIDAD", "CLAVE_MERGE"]
+)
+
+base_busqueda = base_busqueda.merge(
+    descripcion,
+    how="left",
+    on=["ENTIDAD", "CLAVE_MERGE"]
+)
+base_busqueda = base_busqueda.merge(
+    df[
+        ["CLAVE_MERGE", "CLAVE"]
+    ].drop_duplicates(),
+    how="left",
+    on="CLAVE_MERGE"
+)
+base_busqueda[col_emitidas] = (
+    base_busqueda[col_emitidas]
+    .fillna(0)
+)
+
+base_busqueda[col_entregadas] = (
+    base_busqueda[col_entregadas]
+    .fillna(0)
+)
+
 
 # =========================
 # FORMATOS
@@ -95,17 +481,84 @@ def fmt_money(x):
 def calcular_metricas(df_estado):
 
     df_estado = df_estado.copy()
+    # =========================
+    # ABASTO
+    # =========================
+
+    df_estado["PIEZAS_INV"] = pd.to_numeric(
+        df_estado["PIEZAS_INV"],
+        errors="coerce"
+    ).fillna(0)
+
+    df_estado["CPM"] = pd.to_numeric(
+        df_estado["CPM"],
+        errors="coerce"
+    ).fillna(0)
+
+    df_estado["NIVEL_ABASTO"] = 0.0
+
+    mask = df_estado["CPM"] > 0
+
+    df_estado.loc[mask, "NIVEL_ABASTO"] = (
+        df_estado.loc[mask, "PIEZAS_INV"] /
+        df_estado.loc[mask, "CPM"]
+    )
+
+    # CLAVES CON INVENTARIO Y CPM 0
+    df_estado.loc[
+        (df_estado["PIEZAS_INV"] > 0) &
+        (df_estado["CPM"] == 0),
+        "NIVEL_ABASTO"
+    ] = 2
+    # =========================
+    # CLASIFICACIÓN
+    # =========================
+
+    def clasificar(x):
+
+        if x == 0:
+            return "Agotado"
+
+        elif x < 1:
+            return "Próx agotarse"
+
+        elif x <= 1.5:
+            return "Bajo"
+
+        elif x <= 5:
+            return "Óptimo"
+
+        else:
+            return "Sobre stock"
+
+    df_estado["CLASIFICACION"] = (
+        df_estado["NIVEL_ABASTO"]
+        .apply(clasificar)
+    )
+
+    df_estado["Emitido"] = (
+        df_estado[col_emitidas]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
 
     df_estado["Emitido"] = pd.to_numeric(
-        df_estado[col_emitidas],
+        df_estado["Emitido"],
         errors="coerce"
     ).fillna(0)
+
+    df_estado["Entregado"] = (
+        df_estado[col_entregadas]
+        .astype(str)
+        .str.replace(",", "", regex=False)
+        .str.strip()
+    )
 
     df_estado["Entregado"] = pd.to_numeric(
-        df_estado[col_entregadas],
+        df_estado["Entregado"],
         errors="coerce"
     ).fillna(0)
-
     df_estado[col_precio] = pd.to_numeric(
         df_estado[col_precio],
         errors="coerce"
@@ -136,6 +589,52 @@ def calcular_metricas(df_estado):
     c_ent = df_estado[df_estado["Entregado"] >= df_estado["Emitido"]][col_clave].nunique()
     c_tran = df_estado[df_estado["Entregado"] < df_estado["Emitido"]][col_clave].nunique()
 
+    inv_total = (
+    df_estado[
+        ["ENTIDAD", "CLAVE", "PIEZAS_INV"]
+    ]
+    .drop_duplicates()
+    ["PIEZAS_INV"]
+    .sum()
+)
+    cpm_total = (
+    df_estado[
+        ["ENTIDAD", "CLAVE", "CPM"]
+    ]
+    .drop_duplicates()
+    ["CPM"]
+    .sum()
+)
+
+    agotado = (
+        df_estado["CLASIFICACION"]
+        == "Agotado"
+    ).sum()
+
+    proximo = (
+        df_estado["CLASIFICACION"]
+        == "Próx agotarse"
+    ).sum()
+
+    bajo = (
+        df_estado["CLASIFICACION"]
+        == "Bajo"
+    ).sum()
+
+    optimo = (
+        df_estado["CLASIFICACION"]
+        == "Óptimo"
+    ).sum()
+
+    sobre = (
+        df_estado["CLASIFICACION"]
+        == "Sobre stock"
+    ).sum()
+
+    nivel_promedio = (
+        df_estado["NIVEL_ABASTO"]
+        .mean()
+    )    
     tabla = pd.DataFrame([
         ["Piezas", fmt(p), fmt(t), fmt(e)],
         ["Claves", fmt(c_total), fmt(c_tran), fmt(c_ent)],
@@ -144,6 +643,14 @@ def calcular_metricas(df_estado):
     ], columns=["Métrica", "Emitido", "En tránsito", "Entregado"])
 
     return {
+        "inv_total": inv_total,
+        "cpm_total": cpm_total,
+        "agotado": agotado,
+        "proximo": proximo,
+        "bajo": bajo,
+        "optimo": optimo,
+        "sobre": sobre,
+        "nivel_promedio": nivel_promedio,
         "p": p,
         "e": e,
         "t": t,
@@ -237,6 +744,19 @@ metricas_general = calcular_metricas(df)
 st.markdown("## 🌎 Resumen general nacional")
 
 g1, g2, g3 = st.columns(3)
+g4, g5, g6 = st.columns(3)
+
+with g4:
+    st.metric(
+        "📦 Inventario",
+        fmt(metricas_general["inv_total"])
+    )
+
+with g5:
+    st.metric(
+        "📈 CPM",
+        fmt(metricas_general["cpm_total"])
+    )
 
 with g1:
     st.metric(
@@ -268,7 +788,126 @@ with col_n2:
     )
 
 st.divider()
+# =========================
+# BUSCADOR DE CLAVE
+# =========================
 
+st.markdown("## 🔎 Buscar clave nacional")
+
+clave_busqueda = st.text_input(
+    "Ingresar clave"
+)
+clave_busqueda = (
+    clave_busqueda
+    .replace(".", "")
+    .replace(" ", "")
+    .strip()
+)
+
+if clave_busqueda:
+
+    df_busqueda = base_busqueda.copy()
+
+    df_busqueda = df_busqueda[
+        df_busqueda["CLAVE"]
+        .astype(str)
+        .str.contains(
+            clave_busqueda,
+            case=False,
+            na=False
+        )
+    ]
+
+    if len(df_busqueda) > 0:
+
+        df_busqueda["NIVEL_ABASTO"] = 0.0
+
+        mask = df_busqueda["CPM"] > 0
+
+        df_busqueda.loc[mask, "NIVEL_ABASTO"] = (
+            df_busqueda.loc[mask, "PIEZAS_INV"] /
+            df_busqueda.loc[mask, "CPM"]
+        )
+
+        df_busqueda.loc[
+            (df_busqueda["PIEZAS_INV"] > 0) &
+            (df_busqueda["CPM"] == 0),
+            "NIVEL_ABASTO"
+        ] = 2
+
+        def clasificar_busqueda(x):
+
+            if x == 0:
+                return "Agotado"
+
+            elif x < 1:
+                return "Próx agotarse"
+
+            elif x <= 1.5:
+                return "Bajo"
+
+            elif x <= 5:
+                return "Óptimo"
+
+            else:
+                return "Sobre stock"
+
+        df_busqueda["Clasificación"] = (
+            df_busqueda["NIVEL_ABASTO"]
+            .apply(clasificar_busqueda)
+        )
+
+        tabla_busqueda = df_busqueda.copy()
+        tabla_busqueda["Tránsito"] = (
+            tabla_busqueda[col_emitidas] -
+            tabla_busqueda[col_entregadas]
+        ).clip(lower=0)
+
+        tabla_busqueda["Tránsito"] = (
+            tabla_busqueda[col_emitidas] -
+            tabla_busqueda[col_entregadas]
+        ).clip(lower=0)
+
+        tabla_busqueda = tabla_busqueda.rename(columns={
+            "ENTIDAD": "Estado",
+            "CLAVE": "Clave",
+            "PIEZAS_INV": "Inventario",
+            "CPM": "CPM",
+            "NIVEL_ABASTO": "Nivel",
+            "Clasificación": "Clasificación",
+            col_entregadas: "Entregado",
+            col_emitidas: "Emitido"
+        })
+
+        tabla_busqueda["Nivel"] = (
+            tabla_busqueda["Nivel"]
+            .round(2)
+        )
+
+        descripcion = (
+            str(df_busqueda.iloc[0][df.columns[3]])
+        )
+
+        st.markdown(
+            f"### 🔎 Clave encontrada: {descripcion}"
+        )
+
+        tabla_busqueda = tabla_busqueda.sort_values(
+            "Inventario",
+            ascending=False
+        )
+
+        # QUITAR COLUMNAS TÉCNICAS
+        tabla_busqueda = tabla_busqueda.drop(
+            columns=["CLAVE_MERGE"],
+            errors="ignore"
+        )
+
+        st.dataframe(
+            tabla_busqueda,
+            use_container_width=True,
+            hide_index=True
+        )
 # =========================
 # FILTRO
 # =========================
@@ -278,6 +917,78 @@ estado_sel = st.selectbox(
 )
 
 df_f = df[df[col_estado] == estado_sel].copy()
+# =========================
+# RESUMEN ABASTO
+# =========================
+
+df_f = df_f.copy()
+
+df_f["NIVEL_ABASTO"] = 0.0
+
+mask = df_f["CPM"] > 0
+
+df_f.loc[mask, "NIVEL_ABASTO"] = (
+    df_f.loc[mask, "PIEZAS_INV"] /
+    df_f.loc[mask, "CPM"]
+)
+
+df_f.loc[
+    (df_f["PIEZAS_INV"] > 0) &
+    (df_f["CPM"] == 0),
+    "NIVEL_ABASTO"
+] = 2
+
+def clasificar_tabla(x):
+
+    if x == 0:
+        return "Agotado"
+
+    elif x < 1:
+        return "Próx agotarse"
+
+    elif x <= 1.5:
+        return "Bajo"
+
+    elif x <= 5:
+        return "Óptimo"
+
+    else:
+        return "Sobre stock"
+
+df_f["CLASIFICACION"] = (
+    df_f["NIVEL_ABASTO"]
+    .apply(clasificar_tabla)
+)
+abasto_claves = (
+    df_f.groupby(
+        [col_clave],
+        dropna=False
+    )
+    .agg({
+        "PIEZAS_INV": "sum",
+        "CPM": "sum",
+        "NIVEL_ABASTO": "first",
+        "CLASIFICACION": "first"
+    })
+    .reset_index()
+)
+
+abasto_claves.columns = [
+    "Clave",
+    "Inventario",
+    "CPM",
+    "Nivel",
+    "Clasificación"
+]
+
+abasto_claves["Nivel"] = (
+    abasto_claves["Nivel"]
+    .round(2)
+)
+
+abasto_claves = abasto_claves.sort_values(
+    "Nivel"
+)
 
 metricas = calcular_metricas(df_f)
 
@@ -302,6 +1013,24 @@ with col2:
 # =========================
 # VISUALIZACIÓN
 # =========================
+resumen_abasto = (
+    abasto_claves["Clasificación"]
+    .value_counts()
+    .reset_index()
+)
+
+resumen_abasto.columns = [
+    "Clasificación",
+    "Total claves"
+]
+
+st.markdown(f"## 🏥 Resumen de abasto - {estado_sel}")
+
+st.dataframe(
+    resumen_abasto,
+    use_container_width=True,
+    hide_index=True
+)
 st.markdown(f"## 📊 Visualización - {estado_sel}")
 
 c1, c2 = st.columns(2)
